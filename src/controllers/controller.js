5ff22f4b7aa6e5187db21bcc827a0edb4d4cacf0
Lucas <suporte@uept03-02-05.servens.lan>
2023-06-21 21:29:00 -0300

    modelo API MvC get

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,1 +1,26 @@
+const { request, response } = require("../app")
+const db = require("../models/models")
 
+const buscarCor = async (request, response) => {
+    try {
+        let corRequest = request.params.id
+        let cores = db
+        let corEncontrada = []
+        for (let cor of cores) {
+            if (cor.name.toLowerCase().includes(corRequest.toLowerCase())) {
+                corEncontrada.push(cor)
+                console.log(".")
+            }
+            else { }
+        }
+        if (corEncontrada == undefined) throw new Error("Não Encontrada")
+        console.log(corEncontrada)
+        response.status(200).send(corEncontrada)
+    } catch (erro) {
+        response.status(404).json({ message: erro.message })
+    }
+}
+module.exports = {
+    buscarCor
+
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,1 +1,8 @@
+const express = require("express")
+const controller = require("../controllers/controller")
 
+const router = express.Router()
+
+router.get("/busca/:id", controller.buscarCor)
+
+module.exports = router
